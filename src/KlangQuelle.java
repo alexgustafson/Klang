653479f39000e8586697e7fe88564302f957f619
alexgustafson <alexg.hsz@gmail.com>
2012-03-26 23:17:42 +0200

    init commit

---
+++ src/KlangQuelle.java
@@ -0,0 +1,26 @@
+import com.jsyn.unitgen.UnitOscillator;
+
+
+public class KlangQuelle extends UnitOscillator {
+
+	
+	
+	@Override
+	public void generate(int start, int limit) {
+		
+		double[] outputs = output.getValues();
+		System.out.println("start: " + start + "  limit: " + limit);
+		for( int i = start; i < limit; i++)
+		{
+			
+
+				
+				outputs[i] = Math.sin( (double)i/200.0  ) * 20;
+				
+
+			
+		}
+
+	}
+
+}
